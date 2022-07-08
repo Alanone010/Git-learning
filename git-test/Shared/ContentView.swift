833c315eb0baa50954fe1cc7b408baf9ca862167
Alanone010 <528564161@qq.com>
2022-07-08 15:57:43 +0800

    First Commit

---
+++ git-test/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 杨自豪 on 2022/7/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ git-test/Shared/git_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_testApp.swift
+//  Shared
+//
+//  Created by 杨自豪 on 2022/7/8.
+//
+
+import SwiftUI
+
+@main
+struct git_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
